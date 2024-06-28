4404cd25fef6a59bdfeca960e992307aa5dec56d
Ellis Massey <ellisroxit@gmail.com>
2024-06-28 10:50:46 -0500

    Sprint 7 Second Attempt

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-43ff10d5-715e-4328-9f1c-a153550ccf83.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-802d31f3-78b9-48e5-98e4-3df56a816f0b.containerhub.tripleten-services.com"
 }
